057cb494aa7fbf32bf70c1891dc265928205a1de
Alexander Karlis <alexanderkarlis@gmail.com>
2018-08-16 17:06:35 -0400

    added file check using os package

--- breastcancer.py
+++ breastcancer.py
@@ -1,7 +1,17 @@
 import numpy as np 
 import pandas as pd 
+import os
 
-bcdata = pd.read_csv('breast-data.csv', header=None)    
+if True:
+	try:
+		data = 'data/'
+		bcdata = os.listdir(data)
+		print('Data found locally.')
+	except:
+		link = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer.data'
+		bcdata = pd.read_csv(link, header=None)
+		print('Data acquired from remote repo {}'.format(link))    
+ 
 bcdata.columns = ['Class', 'Age', 'Menopause', 'Tumor-Size','Inv-nodes',
  'Node-caps', 'Deg-malig', 'Breast', 'Breast-quad', 'Irradiant']
 bcvals = bcdata.values
